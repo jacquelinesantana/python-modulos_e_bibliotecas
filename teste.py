c341cedd867ff96ad99af174172ef9e555c01be8
jacquelinesantana <ti.jacque@gmail.com>
2024-12-03 13:52:03 -0300

    testes

---
+++ teste.py
@@ -0,0 +1,16 @@
+def add_numbers(a : int , b : int ) -> int:
+    print('somamos aqui')
+    return a + b
+    
+    
+def Subtrai(a: int,b: int) -> int:
+    
+    print('subtraimos aqui')
+    return a - b
+    
+    
+Valores
+VALOR = Subtrai(500 ,150)#CORRETO
+	print( VALOR )
+add_numbers( 1, 3 )#CORRETO
+add_numbers( 1, "2" )#INCORRETO: deve dar erro
